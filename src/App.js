357d282be7df634724d76e09ed1dabdbee844834
wangao029@163.com <wangao029@163.com>
2018-03-13 14:22:10 +0800

    增加 antd 的支持，增加 babel-plugin-import 的支持，并配置按需加载组件

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Button } from 'antd';
+import {DatePicker} from 'antd'
+
 class App extends Component {
   render() {
     return (
@@ -11,7 +14,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <Button>hello</ Button>
+        <DatePicker></DatePicker>
         </p>
       </div>
     );
