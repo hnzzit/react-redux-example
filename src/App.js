a4e9954aa60516b66c5c8ddebc7887ad66b847d0
wangao029@163.com <wangao029@163.com>
2018-03-18 15:32:05 +0800

    增加菜单

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  Main from'./main';
 
-import { Button } from 'antd';
-import {DatePicker} from 'antd'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-        <Button>hello</ Button>
-        <DatePicker></DatePicker>
-        </p>
+       <Main/>
       </div>
     );
   }
